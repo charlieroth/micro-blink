81775084c1a0a677959be76791e1a3a62da1e2ae
charlieroth <charlieroth4@gmail.com>
2025-01-09 21:35:09 +0100

    bsp blinky

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,25 @@
 #![no_std]
 #![no_main]
 
-use cortex_m::asm::nop;
 use cortex_m_rt::entry;
-use embedded_hal::digital::{OutputPin, PinState};
-use hal::gpio::Level;
-use nrf52833_hal::{
-    self as hal,
-    gpio::{
-        p0::{P0_21, P0_28},
-        Output, PushPull,
-    },
-};
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
+use microbit::{board::Board, hal::timer::Timer};
 use panic_halt as _;
 use rtt_target::{rprintln, rtt_init_print};
-extern crate cortex_m;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     rprintln!("Starting...");
-
-    let p = hal::pac::Peripherals::take().unwrap();
-    let port0 = hal::gpio::p0::Parts::new(p.P0); // Get `P0`, 0x5000_0000
-    let _col1: P0_28<Output<PushPull>> = port0.p0_28.into_push_pull_output(Level::Low); // Ground `PIN_CNF[28]`
-    let mut row1: P0_21<Output<PushPull>> = port0.p0_21.into_push_pull_output(Level::Low); // Ground `PIN_CNF[28]`, make mutable
-
-    // blinking loop
-    let mut is_on: bool = false;
+    let mut board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let _ = board.display_pins.col1.set_low();
+    let mut row1 = board.display_pins.row1;
     loop {
-        let _ = row1.set_state(PinState::from(is_on));
-        for _ in 0..400_000 {
-            nop();
-        }
-        is_on = !is_on;
-        if is_on {
-            rprintln!("BLINK");
-        }
+        let _ = row1.set_low();
+        timer.delay_ms(400);
+        let _ = row1.set_high();
+        rprintln!("BLINK");
+        timer.delay_ms(400);
     }
 }
